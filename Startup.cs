86b6ce0d4a3c5ee669999bf49905bb9e3efe0e93
Eric Lacroix <lacroixe@residentsystem.com>
2020-01-08 20:23:22 -0500

    Update css file

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace AppSettings
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
